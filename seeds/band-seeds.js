75fa1b55dcb40b996b8b1063ca656ed78bf63783
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-05-23 20:48:12 -0400

    added images to band cards

--- seeds/band-seeds.js
+++ seeds/band-seeds.js
@@ -5,19 +5,25 @@ const bandData = [
     bandname: "Sick Sugar",
     email: "sick-sugar@music.com",
     imagePath:
-      "https://media.istockphoto.com/photos/hands-in-the-air-picture-id833572242?b=1&k=20&m=833572242&s=170667a&w=0&h=gpxJO3MQKDtpkae9z9ww1Rxn5-Lx9ZaJuS32fn7GSA0=",
+      "https://images.unsplash.com/photo-1467779009031-53938b78ca38?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
   },
   {
     bandname: "Soul Singers",
     email: "soul-singers@music.com",
+    imagePath:
+      "https://images.unsplash.com/photo-1597052826387-65a5a9639944?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074",
   },
   {
     bandname: "Little Monstors",
     email: "little-monstors@music.com",
+    imagePath:
+      "https://images.unsplash.com/photo-1583470037890-73cc6850b7eb?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631",
   },
   {
     bandname: "Fire Wings",
     email: "fire-wings@music.com",
+    imagePath:
+      "https://images.unsplash.com/photo-1503853585905-d53f628e46ac?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686",
   },
 ];
 
