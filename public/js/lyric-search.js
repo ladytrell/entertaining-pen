e8d75e0603ea8e3ed7a66d900a5f08a1f846ac1a
Lady Trell <antrellkent@gmail.com>
2022-05-26 08:13:39 -0400

    Add title and artist to song lyric display

--- public/js/lyric-search.js
+++ public/js/lyric-search.js
@@ -8,21 +8,8 @@ const lyricFormHandler = async function (event) {
   if (songTitleEl && artistEl) {
     const songTitle = songTitleEl.value.trim();
     const artist = artistEl.value.trim();
-    const searchStr = songTitle + ' - ' + artist;
-    location.replace(`/song?search=${searchStr}`);
-   /* 
-    const response = await fetch('/song', {
-      method: 'POST',
-      body: JSON.stringify({
-        search: searchStr,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      console.log(response);
-     // location.replace('/song');
-    }*/
+    // Directing to a new page to display the lyrics
+    location.replace(`/song?title=${songTitle}&artist=${artist}`);
   }
 };
 
